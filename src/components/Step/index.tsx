22bb106e6c65c31ef715e1a4bb8627f318481dff
rikray1208 <rosticpotapov@gmail.com>
2023-02-21 16:54:22 +0100

    step eslint fix

--- src/components/Step/index.tsx
+++ src/components/Step/index.tsx
@@ -1,6 +1,7 @@
-import React, {FC} from 'react';
-import {StepContent, StepDescription, StepIcon, StepItem, StepLine, StepTitle, StepWrapper,} from "./StyledStep";
-import {Check} from "lucide-react";
+import { Check } from 'lucide-react';
+import React, { FC } from 'react';
+
+import { StepContent, StepDescription, StepIcon, StepItem, StepLine, StepTitle, StepWrapper } from './StyledStep';
 export interface StepElement {
     title: string;
     description?: string;
@@ -13,14 +14,14 @@ export interface StepProps {
     small?: boolean;
 }
 export enum State {
-    LESS=-1,
-    CURRENT=0,
-    GREATER=1
+    LESS = -1,
+    CURRENT = 0,
+    GREATER = 1,
 }
 const Step: FC<StepProps> = (props) => {
-    const {current,vertical = false, steps, small = false} =  props;
+    const { current, vertical = false, steps, small = false } = props;
     function validatedNumber(index: number): string {
-        return index < 10 ? " " + index + " " : index + "";
+        return index < 10 ? ' ' + index + ' ' : index + '';
     }
     function stepState(index: number): number {
         if (index < current) return State.LESS;
@@ -30,60 +31,41 @@ const Step: FC<StepProps> = (props) => {
     return (
         <>
             <StepWrapper vertical={vertical}>
-                {
-                    steps.map((step, index) =>
-                        <StepItem
-                            id={'item'}
-                            key={step.title}
-                            style={index == steps.length - 1 ? {flex: '0 1 0'} : {}}
+                {steps.map((step, index) => (
+                    <StepItem
+                        id={'item'}
+                        key={step.title}
+                        style={index == steps.length - 1 ? { flex: '0 1 0' } : {}}
+                        vertical={vertical}
+                    >
+                        <StepIcon
+                            id={'step number'}
+                            isIcon={index < current}
+                            small={small}
+                            state={stepState(index)}
                             vertical={vertical}
                         >
-                            <StepIcon
-                                id={'step number'}
-                                isIcon={index < current}
+                            {index < current ? <Check size={small ? 21 : 24} /> : validatedNumber(index + 1)}
+                        </StepIcon>
+                        <StepContent id={'content'} isLast={index == steps.length - 1} vertical={vertical}>
+                            <StepTitle id={'title'} small={small} state={stepState(index)}>
+                                {step.title}
+                            </StepTitle>
+                            <StepDescription
+                                id={'description'}
                                 small={small}
                                 state={stepState(index)}
                                 vertical={vertical}
                             >
-                                {index < current ?
-                                    <Check size={small ? 21 : 24}/>
-                                    :
-                                    validatedNumber(index + 1)
-                                }
-                            </StepIcon>
-                            <StepContent
-                                id={'content'}
-                                isLast={index == steps.length - 1}
-                                vertical={vertical}
-                            >
-                                <StepTitle
-                                    id={'title'}
-                                    small={small}
-                                    state={stepState(index)}
-                                >
-                                    {step.title}
-                                </StepTitle>
-                                <StepDescription
-                                    id={'description'}
-                                    small={small}
-                                    state={stepState(index)}
-                                    vertical={vertical}
-                                >
-                                    {step.description}
-                                </StepDescription>
-                            </StepContent>
+                                {step.description}
+                            </StepDescription>
+                        </StepContent>
 
-                            {index !== steps.length - 1 &&
-                                <StepLine
-                                    id={'step line'}
-                                    small={small}
-                                    vertical={vertical}
-                                    state={stepState(index + 1)}
-                                />
-                            }
-                        </StepItem>
-                    )
-                }
+                        {index !== steps.length - 1 && (
+                            <StepLine id={'step line'} small={small} vertical={vertical} state={stepState(index + 1)} />
+                        )}
+                    </StepItem>
+                ))}
             </StepWrapper>
         </>
     );
--- src/components/Step/tests/Step.test.tsx
+++ src/components/Step/tests/Step.test.tsx
@@ -1,46 +1,56 @@
-import React from "react";
-import 'jest-styled-components'
-import {ThemeProvider} from "../../../index";
-import Step, {StepElement} from "../index";
-import {create} from "react-test-renderer";
+import React from 'react';
+
+import 'jest-styled-components';
+import { create } from 'react-test-renderer';
+
+import { ThemeProvider } from '../../../index';
+import Step, { StepElement } from '../index';
 
 const Steps: StepElement[] = [
-    {title: 'Login', description:'This is a description'},
-    {title: 'Password asd', description:'This is a description'},
-    {title: 'Auth Code', description:'This is a description'},
-]
+    { title: 'Login', description: 'This is a description' },
+    { title: 'Password asd', description: 'This is a description' },
+    { title: 'Auth Code', description: 'This is a description' },
+];
 
 describe('Step', () => {
-    it('should return a Step', () =>{
+    it('should return a Step', () => {
         const element = (
-            <ThemeProvider><Step current={0} steps={Steps}/></ThemeProvider>
+            <ThemeProvider>
+                <Step current={0} steps={Steps} />
+            </ThemeProvider>
         );
         expect(create(element).toJSON()).toMatchSnapshot();
-    })
-    it('should return a Step with one success element', () =>{
+    });
+    it('should return a Step with one success element', () => {
         const element = (
-            <ThemeProvider><Step current={1} steps={Steps}/></ThemeProvider>
+            <ThemeProvider>
+                <Step current={1} steps={Steps} />
+            </ThemeProvider>
         );
         expect(create(element).toJSON()).toMatchSnapshot();
-    })
-    it('should return a small Step', () =>{
+    });
+    it('should return a small Step', () => {
         const element = (
-            <ThemeProvider><Step current={0} steps={Steps} small={true}/></ThemeProvider>
+            <ThemeProvider>
+                <Step current={0} steps={Steps} small={true} />
+            </ThemeProvider>
         );
         expect(create(element).toJSON()).toMatchSnapshot();
-    })
-    it('should return a vertical Step', () =>{
+    });
+    it('should return a vertical Step', () => {
         const element = (
-            <ThemeProvider><Step current={0} steps={Steps} vertical={true}/></ThemeProvider>
+            <ThemeProvider>
+                <Step current={0} steps={Steps} vertical={true} />
+            </ThemeProvider>
         );
         expect(create(element).toJSON()).toMatchSnapshot();
-    })
-    it('should return a small vertical Step', () =>{
+    });
+    it('should return a small vertical Step', () => {
         const element = (
-            <ThemeProvider><Step current={0} steps={Steps} vertical={true} small={true}/></ThemeProvider>
+            <ThemeProvider>
+                <Step current={0} steps={Steps} vertical={true} small={true} />
+            </ThemeProvider>
         );
         expect(create(element).toJSON()).toMatchSnapshot();
-    })
-})
-
-
+    });
+});
