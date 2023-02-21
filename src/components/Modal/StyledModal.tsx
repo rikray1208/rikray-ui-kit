bbb8b0209c82cd18e70da5c7e4be6111ad4380ce
rikray1208 <rosticpotapov@gmail.com>
2023-02-21 16:50:02 +0100

    modal eslint fix

--- src/components/Modal/StyledModal.tsx
+++ src/components/Modal/StyledModal.tsx
@@ -1,78 +1,80 @@
-import styled, {css, keyframes} from "styled-components";
-import {setFontSize, setShape} from "../../utils/helpers";
-import {ModalProps} from "./index";
-import {Shape} from "../types";
+import styled, { css, keyframes } from 'styled-components';
 
-type WrapperProps = Pick<ModalProps, 'width' | 'shape'>
+import { ModalProps } from './index';
+
+import { setFontSize, setShape } from '../../utils/helpers';
+import { Shape } from '../types';
+
+type WrapperProps = Pick<ModalProps, 'width' | 'shape'>;
 export const Overlay = styled.div`
-  ${({theme}) => css`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width:100%;
-    height: 100%;
-    background: ${theme.Modal.colorOverlay};
-    z-index: ${theme.Modal.zIndexPopup};
+    ${({ theme }) => css`
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: ${theme.Modal.colorOverlay};
+        z-index: ${theme.Modal.zIndexPopup};
 
-    animation: ${fadeIn} ${theme.Modal.durationBase + 'ms'} ${theme.Modal.transitionEaseInOut} forwards;
-    &&.fade-out {
-      animation: ${fadeOut} ${theme.Modal.durationBase + 'ms'} ${theme.Modal.transitionEaseInOut} forwards;
-    }
-  `}
-`
+        animation: ${fadeIn} ${theme.Modal.durationBase + 'ms'} ${theme.Modal.transitionEaseInOut} forwards;
+        &&.fade-out {
+            animation: ${fadeOut} ${theme.Modal.durationBase + 'ms'} ${theme.Modal.transitionEaseInOut} forwards;
+        }
+    `}
+`;
 export const StyledModal = styled.div<WrapperProps>`
-  ${({theme, width, shape}) => css`
-    padding: ${theme.Modal["padding-4"] + 'rem'};
-    position:fixed;
-    top:50%;
-    left:50%;
-    height: auto;
-    width: 100%;
-    max-width: ${width ? width + 'px' : theme.Modal.defaultWidth + 'px'};
-    z-index: ${theme.Modal.zIndexPopup};
-    
-    ${setFontSize('Medium', 'Modal', {})};
+    ${({ theme, width, shape }) => css`
+        padding: ${theme.Modal['padding-4'] + 'rem'};
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        height: auto;
+        width: 100%;
+        max-width: ${width ? width + 'px' : theme.Modal.defaultWidth + 'px'};
+        z-index: ${theme.Modal.zIndexPopup};
 
-    box-shadow: ${theme.Modal.boxShadowPopup};
-    background: ${theme.Modal.colorBgContainer};
-    ${setShape(shape as Shape, 'Modal')};
+        ${setFontSize('Medium', 'Modal', {})};
 
-    animation: ${transformIn} ${theme.Modal.durationBase + 'ms'} ${theme.Modal.transitionEaseInOut} forwards;
-    &&.transform-out {
-      animation: ${transformOut} ${theme.Modal.durationBase + 'ms'} ${theme.Modal.transitionEaseInOut} forwards;
-    };
-    @media(max-width: ${width + 'px'}) {
-      max-width: calc(100vw - ${theme.Modal.screenMargin + 'px'});
-    }
-  `};  
-`
+        box-shadow: ${theme.Modal.boxShadowPopup};
+        background: ${theme.Modal.colorBgContainer};
+        ${setShape(shape as Shape, 'Modal')};
+
+        animation: ${transformIn} ${theme.Modal.durationBase + 'ms'} ${theme.Modal.transitionEaseInOut} forwards;
+        &&.transform-out {
+            animation: ${transformOut} ${theme.Modal.durationBase + 'ms'} ${theme.Modal.transitionEaseInOut} forwards;
+        }
+        @media (max-width: ${width + 'px'}) {
+            max-width: calc(100vw - ${theme.Modal.screenMargin + 'px'});
+        }
+    `};
+`;
 export const Header = styled.div`
-  ${({theme}) => css`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: ${theme.Modal["margin-3"]};
-    ${setFontSize('Large', 'Modal', {})};
-    font-weight: ${theme.Modal.fontWeightBold};
-  `}
-`
+    ${({ theme }) => css`
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: ${theme.Modal['margin-3']};
+        ${setFontSize('Large', 'Modal', {})};
+        font-weight: ${theme.Modal.fontWeightBold};
+    `}
+`;
 
 export const Title = styled.div`
-  ${({theme}) => css`
-    display: flex;
-    gap: ${theme.Modal["padding-1"] + 'rem'};
-    align-items: center;
-    margin-right: auto;
-  `}
-`
+    ${({ theme }) => css`
+        display: flex;
+        gap: ${theme.Modal['padding-1'] + 'rem'};
+        align-items: center;
+        margin-right: auto;
+    `}
+`;
 export const Footer = styled.div`
-  ${({theme}) => css`
-    display: flex;
-    justify-content: end;
-    gap: ${theme.Modal["padding-3"] + 'rem'};
-    margin-top: ${theme.Modal["margin-3"]};
-  `}
-`
+    ${({ theme }) => css`
+        display: flex;
+        justify-content: end;
+        gap: ${theme.Modal['padding-3'] + 'rem'};
+        margin-top: ${theme.Modal['margin-3']};
+    `}
+`;
 const fadeIn = keyframes`
   from {
     opacity: 0;
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,101 +1,105 @@
-import React, {FC, PropsWithChildren, useEffect, useRef} from 'react';
-import {Footer, Header, Overlay, StyledModal, Title} from "./StyledModal";
-import {X} from "lucide-react";
-import Button, {ButtonProps} from "../Button";
-import {Shape} from "../types";
-import {createPortal} from "react-dom";
-import styled from "styled-components";
+import { X } from 'lucide-react';
+import React, { FC, PropsWithChildren, useEffect, useRef } from 'react';
+
+import { createPortal } from 'react-dom';
+
+import { Footer, Header, Overlay, StyledModal, Title } from './StyledModal';
+
+import Button, { ButtonProps } from '../Button';
+import { Shape } from '../types';
+
 export interface ModalProps {
-    open: boolean
-    title?: React.ReactNode,
-    titleIcon?: React.ReactNode,
-    closeIcon?: React.ReactNode,
+    open: boolean;
+    title?: React.ReactNode;
+    titleIcon?: React.ReactNode;
+    closeIcon?: React.ReactNode;
     onConfirm?: (e: React.MouseEvent<HTMLButtonElement | HTMLDivElement>) => void;
-    onCancel?: (e: React.MouseEvent<HTMLButtonElement | HTMLDivElement>) => void,
-    width?: number,
-    shape?: Shape,
-    cancelText?: string,
-    confirmText?: string,
-    overlay?: boolean,
-    btnClose?: Omit<ButtonProps, 'onClick'>,
-    btnCansel?: Omit<ButtonProps, 'onClick'>,
-    btnConfirm?: Omit<ButtonProps, 'onClick'>,
+    onCancel?: (e: React.MouseEvent<HTMLButtonElement | HTMLDivElement>) => void;
+    width?: number;
+    shape?: Shape;
+    cancelText?: string;
+    confirmText?: string;
+    overlay?: boolean;
+    btnClose?: Omit<ButtonProps, 'onClick'>;
+    btnCansel?: Omit<ButtonProps, 'onClick'>;
+    btnConfirm?: Omit<ButtonProps, 'onClick'>;
 }
 const Modal: FC<PropsWithChildren<ModalProps>> = (props) => {
-    const {open, title, onCancel, onConfirm, children, width, shape, titleIcon, cancelText, confirmText, closeIcon, overlay, btnCansel, btnConfirm, btnClose} = props
+    const {
+        open,
+        title,
+        onCancel,
+        onConfirm,
+        children,
+        width,
+        shape,
+        titleIcon,
+        cancelText,
+        confirmText,
+        closeIcon,
+        overlay,
+        btnCansel,
+        btnConfirm,
+        btnClose,
+    } = props;
     const modalRef = useRef<HTMLDivElement | null>(null);
 
-    const Div = styled.div``;
-    const DivError = () => <Div onClick={() => 0} />
     useEffect(() => {
         const EscHandler = (event: KeyboardEvent) => {
             if (event.code === 'Escape') {
                 onCancel && onCancel({} as React.MouseEvent<HTMLDivElement>);
             }
-        }
+        };
 
         modalRef.current?.focus();
-        modalRef.current?.addEventListener("keydown", EscHandler, true);
+        modalRef.current?.addEventListener('keydown', EscHandler, true);
 
         return () => {
-            modalRef.current?.removeEventListener("keydown", EscHandler)
-        }
-    },  [open]);
+            modalRef.current?.removeEventListener('keydown', EscHandler);
+        };
+    });
 
     return (
         <>
-            {open && createPortal(
-                <div tabIndex={0} ref={modalRef}>
-                    {overlay &&
-                        <Overlay
-                            id='rikray-overlay'
-                            className={!open ? 'fade-out' : ''}
-                            onClick={onCancel}
-                        />
-                    }
-                    <StyledModal
-                        id='rikray-modal'
-                        className={!open ? 'transform-out' : ''}
-                        onClick={(e) => e.stopPropagation()}
-                        width={width}
-                        shape={shape}
-                    >
-                        <Header>
-                            <Title>
-                                {titleIcon  && titleIcon}
-                                {title}
-                            </Title>
-                            <Button
-                                shape={shape}
-                                variant={'Text'}
-                                onClick={onCancel}
-                                icon={closeIcon ? closeIcon : <X width={22}/>}
-                                {...btnClose}
-                            />
-                        </Header>
-                        {children}
-                        <Footer>
-                            <Button
-                                shape={shape}
-                                onClick={onCancel}
-                                variant={"Outline"}
-                                {...btnCansel}
-                            >
-                                {cancelText}
-                            </Button>
-                            <Button
-                                shape={shape}
-                                onClick={onConfirm}
-                                {...btnConfirm}
-                            >
-                                {confirmText}
-                            </Button>
-                        </Footer>
-                    </StyledModal>
-                </div>
-                ,
-                document.body)
-            }
+            {open &&
+                createPortal(
+                    <div role={'button'} tabIndex={0} ref={modalRef}>
+                        {overlay && (
+                            <Overlay id='rikray-overlay' className={!open ? 'fade-out' : ''} onClick={onCancel} />
+                        )}
+                        <StyledModal
+                            id='rikray-modal'
+                            className={!open ? 'transform-out' : ''}
+                            onClick={(e) => e.stopPropagation()}
+                            width={width}
+                            shape={shape}
+                        >
+                            <Header>
+                                <Title>
+                                    {titleIcon && titleIcon}
+                                    {title}
+                                </Title>
+                                <Button
+                                    shape={shape}
+                                    variant={'Text'}
+                                    onClick={onCancel}
+                                    icon={closeIcon ? closeIcon : <X width={22} />}
+                                    {...btnClose}
+                                />
+                            </Header>
+                            {children}
+                            <Footer>
+                                <Button shape={shape} onClick={onCancel} variant={'Outline'} {...btnCansel}>
+                                    {cancelText}
+                                </Button>
+                                <Button shape={shape} onClick={onConfirm} {...btnConfirm}>
+                                    {confirmText}
+                                </Button>
+                            </Footer>
+                        </StyledModal>
+                    </div>,
+                    document.body,
+                )}
         </>
     );
 };
@@ -105,5 +109,5 @@ Modal.defaultProps = {
     overlay: true,
     confirmText: 'Ok',
     cancelText: 'Cansel',
-}
+};
 export default Modal;
